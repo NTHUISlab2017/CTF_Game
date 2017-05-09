eb4ede66651235f1e9673faefa5b0320427115b6
duckll <a347liao@gmail.com>
2017-05-09 19:54:00 +0800

    init

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+$dsn='mysql:dbname=ctf;host=localhost';
+$user='ctf';
+$password='ctf';
+try{
+	$db=new PDO($dsn,$user,$password);
+	$db->exec("set names utf8");
+}
+catch(PDOException $e){
+	die($e->getMessage());
+}
+?>
