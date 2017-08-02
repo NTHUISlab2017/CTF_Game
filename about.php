56d90e3aa6c37716e8484d2bfbdaeffc69e45318
楊澤仁 <gcobs61905@gmail.com>
2017-08-02 19:24:16 +0800

    not yet 60%~70%

---
+++ about.php
@@ -0,0 +1,87 @@
+<!-- Bootstrap CSS -->
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  <style type="text/css">
+    .problems {
+      background-color:	#00DBDB;
+      width:100%;
+      height:150;
+    }
+    .rowh{
+      height:170;
+    }
+    .containerr{
+      width: 900px;
+      max-width: 100%;
+    }
+	.problemDes{
+	  min-height:150;
+	  word-wrap:break-word;
+	}
+    
+  </style> 
+</head>
+
+<body>
+  
+
+<?php
+    require_once('config/database.php');
+    
+    $sql = "SELECT COUNT(*)as c FROM Announcement";
+    $stm = $pdo->prepare($sql);
+    $stm -> execute();
+    $sum = $stm->fetch(PDO::FETCH_ASSOC);
+    
+    $sql = "SELECT * FROM Announcement";
+    $stm = $pdo->prepare($sql);
+    $stm -> execute();
+    $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+    
+    ?>
+
+
+<?php
+    require_once('config/database.php');
+    
+    $sql = "SELECT COUNT(*)as c FROM Announcement";
+    $stm = $pdo->prepare($sql);
+    $stm -> execute();
+    $sum = $stm->fetch(PDO::FETCH_ASSOC);
+    
+    $sql = "SELECT * FROM Announcement";
+    $stm = $pdo->prepare($sql);
+    $stm -> execute();
+    $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+    
+    ?>
+
+<h3>Announcement</h3>
+<hr>
+
+<!-- jQuery first, then Tether, then Bootstrap JS. -->
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+<table id='t' style="width: 100%" border = "1">
+<colgroup>
+<col style="width:60px">
+<col style="width:60px">
+<col style="width:450px">
+<col style="width:100px">
+</colgroup>
+<thead>
+<tr id='r'>
+<th>Date</th><th>Type</th><th>Announcement</th><th>Flag</th>
+</tr>
+<tr id = 'r'>
+<td><?php echo $row['Date']?></td>
+<td><?php echo $row['Type']?></td>
+<td><?php echo $row['Announcement']?></td>
+<td><?php echo $row['Flag']?></td>
+</thead>
+
+
+</table>
+<div id='disp'></div>
