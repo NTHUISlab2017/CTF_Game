c4bee63ac314b32794ae8cefd798e21de23082aa
tim70036 <tim70036@gmail.com>
2017-09-24 13:04:49 +0800

    user fix

--- admin/user.php
+++ admin/user.php
@@ -18,7 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 
 		/* Redirect */
 	    header('HTTP/1.1 302 Redirect'); 
-	    header('Location: member.php');
+	    header('Location: user.php');
 	}
 	catch(Exception $e)
 	{
@@ -31,7 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 <html>
  	<!-- Bootstrap CSS -->
     <head>
-    <title>member</title>
+    <title>user</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
       <style type="text/css">
         .problems {
@@ -47,7 +47,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 
-    	<form method="post" action="member.php">
+    	<form method="post" action="user.php">
 	    	<table class="table table-hover">
 			    <thead>
 			      <tr>
